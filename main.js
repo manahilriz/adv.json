0bb00ce767381711643c47392a6bbf6c2f0c6d75
manahilriz <128300856+manahilriz@users.noreply.github.com>
2023-05-23 23:14:24 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+//const sum =(a)=> {
+    //let sum = 0
+   // for(let i of a){
+     // sum += i;
+   // }
+   // console.log(sum);
+//}
+
+//let num =[34,22,11,10,44,55,85,70,23,100];
+
+//sum(num);
+
+//const sum =(...a) => {
+  //  let sum = 0
+    //for (let i of a){
+      //  sum +=i;
+    //}
+    //console.log(sum);
+//}
+
+//sum(11,13,12,16,10,23,55,46,99);
